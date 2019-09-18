01a0fe9865d8a1afda326614e2a503604c7d3362
idan <idanelbaz@gmail.com>
2019-09-18 13:06:59 +0300

    Updates

---
+++ precache-manifest.0e76c2c1219a8b27fad4adaf8e25ee3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55cabad26476df7df88d14b6bbf99130",
+    "url": "/Social-React/index.html"
+  },
+  {
+    "revision": "7fea0983c9b8ef7a0ab9",
+    "url": "/Social-React/static/css/2.81cb803e.chunk.css"
+  },
+  {
+    "revision": "8f015d75cc6ae6f840e2",
+    "url": "/Social-React/static/css/main.580ea453.chunk.css"
+  },
+  {
+    "revision": "7fea0983c9b8ef7a0ab9",
+    "url": "/Social-React/static/js/2.94fa2923.chunk.js"
+  },
+  {
+    "revision": "8f015d75cc6ae6f840e2",
+    "url": "/Social-React/static/js/main.6b23c92c.chunk.js"
+  },
+  {
+    "revision": "5fd03983a0a470493e9c",
+    "url": "/Social-React/static/js/runtime~main.52ce7fe8.js"
+  },
+  {
+    "revision": "1c04c2a92b193860bceaf7cb1153ee6b",
+    "url": "/Social-React/static/media/newIcon.1c04c2a9.svg"
+  }
+]);
